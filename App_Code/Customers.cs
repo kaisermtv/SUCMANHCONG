ff5deed0d988647089230d7e1ab2b567c3b4d7a5
kaisermtv <kaisermtv@yahoo.com>
2016-12-09 14:36:45 +0700

    update System/Customer.aspx.cs

--- App_Code/Customers.cs
+++ App_Code/Customers.cs
@@ -11,6 +11,48 @@ public class Customers
 	{
 	}
 
+    #region method getDataCustomer
+    public DataTable getDataCustomer()
+    {
+        DataTable objTable = new DataTable();
+        try
+        {
+
+            SqlConnection sqlCon = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TVSConn"].ConnectionString);
+            sqlCon.Open();
+            SqlCommand Cmd = sqlCon.CreateCommand();
+            Cmd.CommandText = "SELECT 0 AS TT, '' AS State1, *, '' AS NgayTao FROM tblCustomers ORDER BY DayRegister DESC";
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = Cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            sqlCon.Close();
+            sqlCon.Dispose();
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                ds.Tables[0].Rows[i]["TT"] = (i + 1);
+                ds.Tables[0].Rows[i]["NgayTao"] = DateTime.Parse(ds.Tables[0].Rows[i]["DayRegister"].ToString()).ToString("dd/MM/yyyy");
+                if (ds.Tables[0].Rows[i]["State"].ToString().ToUpper() == "TRUE")
+                {
+                    ds.Tables[0].Rows[i]["State1"] = "<B>x</B>";
+                }
+                else
+                {
+                    ds.Tables[0].Rows[i]["State1"] = "-:-";
+                }
+            }
+            objTable = ds.Tables[0];
+
+        }
+        catch
+        {
+
+        }
+
+        return objTable;
+    }
+    #endregion
+
     #region method getCustomer
     public DataTable getCustomer(string Account)
     {
--- System/Customer.aspx.cs
+++ System/Customer.aspx.cs
@@ -9,6 +9,10 @@ using System.Web.UI.WebControls;
 
 public partial class Customer : System.Web.UI.Page
 {
+    #region declare objects
+    private Customers objCustomers = new Customers();
+    #endregion
+
     #region method Page_Load
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -27,34 +31,14 @@ public partial class Customer : System.Web.UI.Page
     #region method getPartner
     public DataTable getPartner()
     {
-        SqlConnection sqlCon = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TVSConn"].ConnectionString);
-        sqlCon.Open();
-        SqlCommand Cmd = sqlCon.CreateCommand();
-        Cmd.CommandText = "SELECT 0 AS TT, '' AS State1, *, '' AS NgayTao FROM tblCustomers ORDER BY DayRegister DESC";
-        SqlDataAdapter da = new SqlDataAdapter();
-        da.SelectCommand = Cmd;
-        DataSet ds = new DataSet();
-        da.Fill(ds);
-        sqlCon.Close();
-        sqlCon.Dispose();
-        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-        {
-            ds.Tables[0].Rows[i]["TT"] = (i + 1);
-            ds.Tables[0].Rows[i]["NgayTao"] = DateTime.Parse(ds.Tables[0].Rows[i]["DayRegister"].ToString()).ToString("dd/MM/yyyy");
-            if (ds.Tables[0].Rows[i]["State"].ToString().ToUpper() == "TRUE")
-            {
-                ds.Tables[0].Rows[i]["State1"] = "<B>x</B>";
-            }
-            else
-            {
-                ds.Tables[0].Rows[i]["State1"] = "-:-";
-            }
-        }
-        if (ds.Tables[0].Rows.Count < 120)
+        DataTable objData = this.objCustomers.getDataCustomer();
+
+        if (objData.Rows.Count < 120)
         {
             this.tblABC.Visible = false;
         }
-        return ds.Tables[0];
+
+        return objData;
     }
     #endregion
 }
