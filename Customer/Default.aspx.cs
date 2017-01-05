2b3ee88ac0e0fcac348e4e5373f0b0c6b05f35a9
hieuxinhe94 <hieu.e.content@gmail.com>
2017-01-05 16:43:48 +0700

    TẠO THƯ MỤC THIẾT KẾ MỚI , CHỈNH GIAO DIỆN TRANG Default.aspx KHÁCH HÀNG

--- Customer/Default.aspx.cs
+++ Customer/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +8,18 @@ using System.Web.UI.WebControls;
 
 public partial class Customer_Default : System.Web.UI.Page
 {
+    #region declare
+    private DataTopic objTopic = new DataTopic();
+    public DataTable objtable = new DataTable();
+    #endregion
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            objtable = objTopic.getTopTopic();
+        }
     }
+
+
 }
