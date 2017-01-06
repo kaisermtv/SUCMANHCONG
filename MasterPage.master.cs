32a931b96eb2fd567868a2aa2e490b2d5078bc09
kaisermtv <kaisermtv@yahoo.com>
2017-01-06 08:44:47 +0700

    add exit admin

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -7,12 +7,15 @@ using System.Web.UI.WebControls;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+    public string Acct = ""; 
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["ADMINLOGIN"] == null || (int)Session["ADMINLOGIN"] == 0)
         {
           Response.Redirect("/system/login");
         }
+
         int buf = (int)Session["ADMINLOGIN"];
 
         Session["ADMINLOGIN"] = buf;
